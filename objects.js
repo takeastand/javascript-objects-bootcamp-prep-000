e3c032181a49494e34ab8ced4926b9fc229201f0
christopher.tero Tero <christopher.tero@gmail.com>
2019-01-10 03:34:36 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {artistName: "songTitle"};
+var playlist = new Object({artistName: 'songTitle'});
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, {artistName: songTitle});
